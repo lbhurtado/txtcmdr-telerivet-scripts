9b8ede107ee953d66781a01ab77a7517529d3ea7
Lester B. Hurtado <lester@applester.co>
2015-09-16 07:59:04 +0800

    Rename main.js to addtogroup.js and used $addtogroup variable

--- addtogroup.js
+++ addtogroup.js
@@ -1,8 +1,7 @@
-var ar = contact.vars.addtogroups.split(",");
-if (ar){
+if ($addtogroups) {
+  var ar = $addtogroups.split(",");
   for (var i in ar) {
     var grp = project.getOrCreateGroup(ar[i]);
     contact.addToGroup(grp);
   }
 }
-contact.vars.addtogroups = null;
