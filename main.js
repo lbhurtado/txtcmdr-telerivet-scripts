4077340f4f3cdc1d3a1526cd8115a5fb72b0872b
Lester B. Hurtado <lester@applester.co>
2015-09-16 07:46:19 +0800

    Add validation if groups is nil

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var ar = contact.vars.addtogroups.split(",");
-for (var i in ar) {
-  var grp = project.getOrCreateGroup(ar[i]);
-  contact.addToGroup(grp);
+if (ar){
+  for (var i in ar) {
+    var grp = project.getOrCreateGroup(ar[i]);
+    contact.addToGroup(grp);
+  }
 }
 contact.vars.addtogroups = null;
