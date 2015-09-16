867a4000c5072b93fb8454ac512b3833c78f7695
Lester B. Hurtado <lester@applester.co>
2015-09-16 17:52:02 +0800

    Add cleanup.js (7)

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ require("ext/applester-scripts/addtogroups");
 require("ext/applester-scripts/addrecruit");
 require("ext/applester-scripts/addmobiletogroups");
 require("ext/applester-scripts/removemobilefromgroups");
-require("ext/applester-scripts/cleanup");
+//require("ext/applester-scripts/cleanup");
 
 console.log('using main.js');
