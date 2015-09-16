1fcf02a030187a143c6abd255907c7624034464c
Lester B. Hurtado <lester@applester.co>
2015-09-16 13:04:27 +0800

    Add removemobilefromgroups.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 require("ext/applester-scripts/addtogroups");
 require("ext/applester-scripts/addrecruit");
 require("ext/applester-scripts/addmobiletogroups");
+require("ext/applester-scripts/removemobilefromgroups");
 
 
 console.log('using main.js');
---
+++ removemobilefromgroups.js
@@ -0,0 +1,32 @@
+/**
+ * Created by lbhurtado on 9/16/15.
+ */
+
+if (typeof $removemobilefromgroups !== 'undefined') {
+    var colon_delimited_text = $removemobilefromgroups.trim();
+    if (colon_delimited_text.length > 0) {
+        var mobile_groups_array = colon_delimited_text.split(":");
+        var mobile = mobile_groups_array[0];
+        var comma_delimited_text = mobile_groups_array[1];
+        var groups_array = comma_delimited_text.split(",");
+
+        var user_group_ids_array = []
+        var user = project.getOrCreateContact({phone_number: mobile});
+        cursor = user.queryGroups();
+        cursor.limit(50);
+        while (cursor.hasNext()) {
+            var group = cursor.next();
+            user_group_ids_array.push({name: group.name, id: group.id});
+        }
+
+        for (var i in groups_array) {
+            var group_name = groups_array[i];
+            var user_group_id = user_group_ids_array[group_name];
+            if (user_group_id) {
+                var group = project.initGroupById(user_group_id);
+                user.removeFromGroup(group);
+            }
+        }
+        console.log('using removemobilefromgroups.js');
+    }
+}
