8323b041b5703d2364e5f0a7b719b3b4ca686dce
Lester B. Hurtado <lester@applester.co>
2015-09-16 13:12:53 +0800

    Add debug info in removemobilefromgroup.js

--- removemobilefromgroups.js
+++ removemobilefromgroups.js
@@ -10,14 +10,21 @@ if (typeof $removemobilefromgroups !== 'undefined') {
         var comma_delimited_text = mobile_groups_array[1];
         var groups_array = comma_delimited_text.split(",");
 
+        console.log(groups_array);
+
         var user_group_ids_array = []
         var user = project.getOrCreateContact({phone_number: mobile});
+
+        console.log(user);
+        
         cursor = user.queryGroups();
         cursor.limit(50);
         while (cursor.hasNext()) {
             var group = cursor.next();
             user_group_ids_array.push({name: group.name, id: group.id});
         }
+
+        console.log(user_group_ids_array);
 
         for (var i in groups_array) {
             var group_name = groups_array[i];
