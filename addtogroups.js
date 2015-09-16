66c24132a15cb808f5d4d7715c67751156f67eb8
Lester B. Hurtado <lester@applester.co>
2015-09-16 10:37:53 +0800

    Add comment and change names of variables to something easy to understand

--- addtogroups.js
+++ addtogroups.js
@@ -1,9 +1,18 @@
+/*
+$addtogroups is a temporary variable created by the 
+the json response of the webhook.  If it has a value, 
+a comma-delimited text of group names will be parsed.
+Each group will be created if doesn't exist yet and 
+then the contact will be added to the group
+*/
+
 if (typeof $addtogroups !== 'undefined') {
-  if ($addtogroups.length > 0) {
-    var ar = $addtogroups.split(",");
-    for (var i in ar) {
-      var grp = project.getOrCreateGroup(ar[i]);
-      contact.addToGroup(grp);
+  var comma_delimited_text = $addtogroups.trim();
+  if (comma_delimited_text.length > 0) {
+    var groups_array = comma_delimited_text.split(",");
+    for (var i in groups_array) {
+      var group = project.getOrCreateGroup(groups_array[i]);
+      contact.addToGroup(group);
     }
   }
 }
