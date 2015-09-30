cafc04f9d30cbd91fbbfaace4affd055ec0a029b
Lester B. Hurtado <lester@applester.co>
2015-09-30 10:59:15 +0800

    Add response to SARILI

--- demo.js
+++ demo.js
@@ -3,29 +3,35 @@ function toTitleCase(str) {
 }
 
 if (!state.id) {
-  if (word1.toUpperCase().indexOf('BAYAN') != -1) {
-    var groupBayan = project.getOrCreateGroup('Bayan');
-    contact.addToGroup(groupBayan);
-    sendReply("Welcome to the mock survey for congressional elections. Get a P10 load for answering 5 questions. Reply with “yes” to proceed.");
-    state.id = 'bayan';
-  }
-  else
-	sendReply("Bayan o sarili?");
+	if (word1.toUpperCase().indexOf('BAYAN') != -1) {
+    	var groupBayan = project.getOrCreateGroup('Bayan');
+    	contact.addToGroup(groupBayan);
+    	sendReply("Welcome to the mock survey for congressional elections. Get a P10 load for answering 5 questions. Reply with “yes” to proceed.");
+    	state.id = 'bayan';
+  	}
+  	else if (word1.toUpperCase().indexOf('SARILI') != -1)
+  		sendReply("To each his own.");
+  	else
+  		sendReply("Bayan o sarili?");
 }
-else if (state.id == 'bayan') {
+elseif (state.id == 'bayan') {
 	if (word1.toUpperCase().indexOf('YES') != -1) {
 		var groupOptIn = project.getOrCreateGroup('OptIn');
 		contact.addToGroup(groupOptIn);
 		sendReply("Thank you. Please text your name.");
 		state.id = 'name';
-  }
+	}
+	else {
+		sendReply("Bayan o sarili?");
+		state.id = null;
+	}
 }
-else if (state.id == 'name') {
+elseif (state.id == 'name') {
 	contact.name = toTitleCase(message.content.replace(/[^\w\s]/gi, '')); //clean up name
 	sendReply("Hi " + contact.name + ". Who among the following is your best choice for Congress in 2016? Select a letter only: 'A' (Juan), 'B' (Pedro), 'C' (Maria))");
 	state.id = 'q1';
 }
-else if (state.id == 'q1') {
+elseif (state.id == 'q1') {
 	var letters = ["A", "B", "C"];
     var choice = letters.indexOf(word1.toUpperCase());
     if (choice != -1) {
@@ -36,7 +42,7 @@ else if (state.id == 'q1') {
     	sendReply("Hi " + contact.name + ", just the letter 'A', 'B' or 'C' only. Who among the following is your best choice for Congress in 2016? Select a letter only: 'A' (Juan), 'B' (Pedro), 'C' (Maria))");
 
 }
-else if (state.id == 'q2') {
+elseif (state.id == 'q2') {
 	var numerals = ["1", "2", "3"];
     var choice = numerals.indexOf(word1);
     if (choice != -1) {
@@ -46,7 +52,7 @@ else if (state.id == 'q2') {
 	else
     	sendReply("Hi " + contact.name + ", just the numeral '1', '2' or '3' only. Why did you choose this candidate? Select a numeral only: '1' (leadership), '2' (program or agenda), '3' (personality)");
 }
-else if (state.id == 'q3') {
+elseif (state.id == 'q3') {
 	var letters = ["A", "B", "C"];
     var choice = letters.indexOf(word1.toUpperCase());
     if (choice != -1) {
