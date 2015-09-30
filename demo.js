f57a75797e931a8bfdc3145bf3781801717ad499
Lester B. Hurtado <lester@applester.co>
2015-09-30 11:01:53 +0800

    Add response to SARILI(3)

--- demo.js
+++ demo.js
@@ -9,12 +9,12 @@ if (!state.id) {
     	sendReply("Welcome to the mock survey for congressional elections. Get a P10 load for answering 5 questions. Reply with “yes” to proceed.");
     	state.id = 'bayan';
   	}
-  	elseif (word1.toUpperCase().indexOf('SARILI') != -1)
+  	else if (word1.toUpperCase().indexOf('SARILI') != -1)
   		sendReply("To each his own.");
   	else
   		sendReply("Bayan o sarili?");
 }
-elseif (state.id == 'bayan') {
+else if (state.id == 'bayan') {
 	if (word1.toUpperCase().indexOf('YES') != -1) {
 		var groupOptIn = project.getOrCreateGroup('OptIn');
 		contact.addToGroup(groupOptIn);
@@ -26,12 +26,12 @@ elseif (state.id == 'bayan') {
 		state.id = null;
 	}
 }
-elseif (state.id == 'name') {
+else if (state.id == 'name') {
 	contact.name = toTitleCase(message.content.replace(/[^\w\s]/gi, '')); //clean up name
 	sendReply("Hi " + contact.name + ". Who among the following is your best choice for Congress in 2016? Select a letter only: 'A' (Juan), 'B' (Pedro), 'C' (Maria))");
 	state.id = 'q1';
 }
-elseif (state.id == 'q1') {
+else if (state.id == 'q1') {
 	var letters = ["A", "B", "C"];
     var choice = letters.indexOf(word1.toUpperCase());
     if (choice != -1) {
@@ -42,7 +42,7 @@ elseif (state.id == 'q1') {
     	sendReply("Hi " + contact.name + ", just the letter 'A', 'B' or 'C' only. Who among the following is your best choice for Congress in 2016? Select a letter only: 'A' (Juan), 'B' (Pedro), 'C' (Maria))");
 
 }
-elseif (state.id == 'q2') {
+else if (state.id == 'q2') {
 	var numerals = ["1", "2", "3"];
     var choice = numerals.indexOf(word1);
     if (choice != -1) {
@@ -52,7 +52,7 @@ elseif (state.id == 'q2') {
 	else
     	sendReply("Hi " + contact.name + ", just the numeral '1', '2' or '3' only. Why did you choose this candidate? Select a numeral only: '1' (leadership), '2' (program or agenda), '3' (personality)");
 }
-elseif (state.id == 'q3') {
+else if (state.id == 'q3') {
 	var letters = ["A", "B", "C"];
     var choice = letters.indexOf(word1.toUpperCase());
     if (choice != -1) {
