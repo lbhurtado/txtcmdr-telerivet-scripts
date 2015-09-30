0fb9d28ac1db81db671a229c9ef71daf9f5a4e40
Lester B. Hurtado <lester@applester.co>
2015-09-30 22:02:30 +0800

    Add statistics(31)

--- demo.js
+++ demo.js
@@ -18,7 +18,9 @@ function updatePoll(vquestion, vanswer) {
 
 var vtable = project.getOrCreateDataTable("DemoPollTable");
 var results = []; 
-cursor = vtable.queryRows();
+cursor = vtable.queryRows({
+    vars: {'question': "q2"}
+});
 cursor.limit(5);
 while (cursor.hasNext()) {
     var row = cursor.next();
