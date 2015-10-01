38d5762d64ba99f50ea8e6290f54814c620e3609
Lester B. Hurtado <lester@applester.co>
2015-10-01 10:38:51 +0800

    Add function poll to demo (8)

--- demo.js
+++ demo.js
@@ -154,7 +154,8 @@ if (!state.id) {
         }
         var poll_text = "";
         var attrib = "";
-        var results = -.sortBy(poll(question),1);
+        var results = poll(question);
+        results = _.sortBy(results, function(num){ return num[1]; });
         for (var i=0,  tot=results.length; i < tot; i++) {
             console.log(results[i]);
             attrib = ar[results[i][0]];
