da343a476e169faf074459d4b3587acfb2db0eaf
Lester B. Hurtado <lester@applester.co>
2015-10-04 10:22:40 +0800

    Add shorthand notations(24)

--- poll.js
+++ poll.js
@@ -115,12 +115,12 @@ var survey = [
         },
         'regex': /^[RBPB]$/,
         'question': function (tries) {
-            contact.vars.tries = ['apple','pear','mango'];
+            contact.vars[tries'] = 537;
 
             //contact,vars.tries[this.state] = 0;
             //contact.vars.tries = contact.vars.tries || 0;
 
-            console.log(contact.vars.tries);
+            console.log(contact.vars[tries']);
             var retval = [
                 _(this.state).capitalize() + ": ",
                 this.template,
