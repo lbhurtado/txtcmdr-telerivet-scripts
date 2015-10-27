697d29b5fb97607ebe13a43af5527fc4cf1f5f89
“Lester <lester@applester.co>
2015-10-27 15:10:53 +0800

    Add scratch/js

--- poll.js
+++ poll.js
@@ -1672,13 +1672,13 @@ console.log("text message = " + message.content);
             return process;
         },
         process = processInput(state.id),
-        report = getReport(keyword),
+        report = getReport(keyword)
 
-        util = require("ext/applester-scripts/string2argv"),
-        optparse = require("ext/applester-scripts/optparse"),
-        PathParser = require("ext/applester-scripts/pathparser.min"),
-        ARGS = util.parseArgsStringToArgv(input),
-        router = new PathParser
+        //util = require("ext/applester-scripts/string2argv"),
+        //optparse = require("ext/applester-scripts/optparse"),
+        //PathParser = require("ext/applester-scripts/pathparser.min"),
+        //ARGS = util.parseArgsStringToArgv(input),
+        //router = new PathParser
         ;
 
     console.log("regex = " + regex);
@@ -1693,8 +1693,8 @@ console.log("text message = " + message.content);
     //console.log("contact telco = " + telco);
     //console.log("syntax = " + syntax);
 
-    //if (keyword) state.id = nextState;
-    state.id = keyword ? nextState : "catchall";
+    if (keyword) state.id = nextState;
+    //state.id = keyword ? nextState : "catchall";
 
     !message || project.sendMessage({
         content: message,
@@ -1702,115 +1702,7 @@ console.log("text message = " + message.content);
         is_template: true
     });
 
-    // Define some options
-    var SWITCHES = [
-        ['-i', '--include-file FILE', "Includes a file"],
-        ['-p', '--print [MESSAGE]', "Prints an optional message on screen"],
-        ['-d', '--debug', "Enables debug mode"],
-        ['-H', '--help', "Shows this help section"],
-        ['--date DATE', "A date. A date is expected E.G. 2009-01-14"],
-        ['--number NUMBER', "A Number. Supported formats are 123, 123.123, 0xA123"],
-        ['--marcos NUMBER', "A Number. Supported formats are 123, 123.123, 0xA123"],
-        ['--escudero NUMBER', "A Number. Supported formats are 123, 123.123, 0xA123"],
-        ['--other NAME', "No handler defined for this option. Will be handled by the wildcard handler."],
-    ];
 
-// Create a new OptionParser with defined switches
-    var
-        parser = new optparse.OptionParser(SWITCHES),
-        print_summary = true,
-        first_arg;
-
-    parser.banner = 'Usage: nodejs-test.js [options]';
-
-// Internal variable to store options.
-    var options = {
-        debug: true,
-        files: [],
-        number: undefined,
-        date: undefined
-    };
-
-// Handle the first argument (switches excluded)
-    parser.on(0, function (value) {
-        first_arg = value;
-    });
-
-// Handle the --include-file switch
-    parser.on('include-file', function (name, value) {
-        options.files.push(value);
-    });
-
-// Handle the --print switch
-    parser.on('print', function (name, value) {
-        console.log('PRINT: ' + (value || 'No message entered'));
-    });
-
-// Handle the --date switch
-    parser.on('date', function (name, value) {
-        options.date = value;
-    });
-
-// Handle the --number switch
-    parser.on('number', function (name, value) {
-        options.number = value;
-    });
-
-// Handle the --debug switch
-    parser.on('debug', function () {
-        options.debug = true;
-    });
-
-// Handle the --help switch
-    parser.on('help', function () {
-        console.log(parser.toString());
-        print_summary = false;
-    });
-
-// Set a default handler
-    parser.on('*', function (opt, value) {
-        console.log('wild handler for ' + opt + ', value=' + value);
-    });
-
-
-    console.log("ARGS = " + ARGS);
-
-// Parse command line arguments
-    parser.parse(ARGS);
-
-    if (print_summary) {
-        console.log("First non-switch argument is: " + first_arg);
-
-        // Output all files that was included.
-        console.log("No of files to include: " + options.files.length);
-        for (var i = 0; i < options.files.length; i++) {
-            console.log("File [" + (i + 1) + "]:" + options.files[i]);
-        }
-
-        // Is debug-mode enabled?
-        console.log("Debug mode is set to: " + options.debug);
-
-        console.log("Number value is: " + options.number);
-        console.log("Date value is: " + options.date);
-    }
-
-    /*
-     console.log("after arguments.parse(ARGS)");
-
-     router.add('collections/:collectionID/items/:subset', function () {
-     console.log(this.collectionID);
-     console.log(this.subset);
-     });
-
-     router.add('join', function () {
-     console.log("router join");
-     });
-
-     console.log("router.add('collections/:collectionID/items/:subset', function () {");
-
-     router.run('collections/3424/items');
-     router.run('join');
-     */
 
 })(congress_demo, message.content);
 
