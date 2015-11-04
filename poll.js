d86ccbd84b195d042b20c7a7cd69ec71b169070e
Lester B. Hurtado <lester@applester.co>
2015-11-04 22:12:43 +0800

    Add passage(15)

--- poll.js
+++ poll.js
@@ -1806,7 +1806,7 @@ console.log("text message = " + message.content);
                             origin = contact.phone_number,
                             destination = "639189362340";
                         var
-                            passage = implode(" ", parameters),
+                            passage = parameters[0] + parameters[1],
                             url = "http://128.199.81.129/txtcmdr/read/" + origin + "/" + destination + "/" + passage,
                             response = httpClient.request(url, {
                                 method: 'POST'
