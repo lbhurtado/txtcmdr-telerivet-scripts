34a75ef1a65d3083f3656f2fa7eea332a01282f3
Lester B. Hurtado <lester@applester.co>
2015-11-06 00:42:19 +0800

    Start anew, use router(5)

--- portal.js
+++ portal.js
@@ -5,7 +5,15 @@
 var PathParser = require("ext/applester-scripts/pathparser.min");
 
 var params = {};
+
 var router = new PathParser(params);
+
+router.add('subscribe/:name', function () {
+    contact.name = this.name;
+    var group = project.getOrCreateGroup('subscriber');
+    contact.addToGroup(group);  
+});
+
 router.add('items/:itemID');
 router.add('collections/:collectionID/items/:itemID');
 
