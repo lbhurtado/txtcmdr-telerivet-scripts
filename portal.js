11bef8518b4ccda559b3b09b5b0cfeedbc9773f0
Lester B. Hurtado <lester@applester.co>
2015-11-06 11:13:31 +0800

    Start anew, use router(41)

--- portal.js
+++ portal.js
@@ -2,52 +2,55 @@
  * Created by lbhurtado on 05/11/15.
  */
 
-_.mixin({
+(function (input) {
+    'use strict';
 
-    capitalize: function (string) {
-        return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
-    },
-    titleCase: function (str) {
-        return str.replace(/\w\S*/g, function (txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
-    },
-    analyzeParams: function (params) {
-        var parts = [];
-        _(params).each(function(param){
-            if (!_.isUndefined(param)) {
-                parts.push(param);
-            }
-        });
-        var input = parts.pop();
-        return {
-            'input': input,
-            'parts': parts
-        };
-    }
-});
+    _.mixin({
 
-var
-    PathParser = require("ext/applester-scripts/pathparser.min"),
-    params = {},
-    router = new PathParser(params);
+        capitalize: function (string) {
+            return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+        },
+        titleCase: function (str) {
+            return str.replace(/\w\S*/g, function (txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+        },
+        analyzeParams: function (params) {
+            var parts = [];
+            _(params).each(function(param){
+                if (!_.isUndefined(param)) {
+                    parts.push(param);
+                }
+            });
+            var input = parts.pop();
+            return {
+                'input': input,
+                'parts': parts
+            };
+        }
+    });
 
-router.add('subscribe/:name1/:name2/:name3/:name4', function () {
-    params.name = _(((_(params).analyzeParams())
-        .parts
-        .join(' '))
-        .replace(/[^\w\s]/gi, ''))
-        .titleCase();
-    params.group = 'subscriber';
-});
+    var
+        util = require("ext/applester-scripts/string2argv"),
+        url = util.parseArgsStringToArgv(input).join('/'),
+        PathParser = require("ext/applester-scripts/pathparser.min"),
+        params = {},
+        router = new PathParser(params);
 
-var
-    util = require("ext/applester-scripts/string2argv"),
-    args = util.parseArgsStringToArgv(message.content),
-    url = args.join('/');
+    router.add('subscribe/:name1/:name2/:name3/:name4', function () {
+        params.name = _(((_(params).analyzeParams())
+            .parts
+            .join(' '))
+            .replace(/[^\w\s]/gi, ''))
+            .titleCase();
+        params.group = 'subscriber';
+    });
 
-router.run(url);
+    router.run(url);
 
-if (params.name) contact.name = params.name;
-if (params.group) contact.addToGroup(project.getOrCreateGroup(params.group));
+    if (params.name) contact.name = params.name;
+    if (params.group) contact.addToGroup(project.getOrCreateGroup(params.group));
+
+}(message.content));
+
 
