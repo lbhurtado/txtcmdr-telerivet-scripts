d9b82229556dcb6aba69e5d081ff88fa802918eb
Lester B. Hurtado <lester@applester.co>
2015-11-06 13:33:48 +0800

    Start anew, use router(44)

--- portal.js
+++ portal.js
@@ -2,7 +2,7 @@
  * Created by lbhurtado on 05/11/15.
  */
 
-(function (input) {
+var params = (function (input, status) {
     'use strict';
 
     _.mixin({
@@ -16,7 +16,7 @@
         },
         analyzeParams: function (params) {
             var parts = [];
-            _(params).each(function(param){
+            _(params).each(function (param) {
                 if (!_.isUndefined(param)) {
                     parts.push(param);
                 }
@@ -31,11 +31,11 @@
 
     var
         argvUtililty = require("ext/applester-scripts/string2argv"),
-        PathParser = require("ext/applester-scripts/pathparser.min"),
-        url = argvUtililty.parseArgsStringToArgv(input).join('/'),
-        params = {},
-        router = new PathParser(params),
-        getNameFromURL = function(params){
+        pathParser = require("ext/applester-scripts/pathparser.min"),
+        generatedURL = argvUtililty.parseArgsStringToArgv(input).join('/'),
+        generatedParams = {},
+        router = new pathParser(generatedParams),
+        generateNameFromURL = function (params) {
             return _(((_(params).analyzeParams())
                 .parts
                 .join(' '))
@@ -44,15 +44,37 @@
         }
 
     router.add('subscribe/:name1/:name2/:name3/:name4', function () {
-        params.name = getNameFromURL(params);
-        params.group = 'subscriber';
+        var
+            name = generateNameFromURL(generatedParams),
+            group = "subscriber",
+            reply = name + " are now a subscriber.",
+            state = null;
+
+        generatedParams.name = name;
+        generatedParams.groups = [group];
+        generatedParams.reply = reply;
+        generatedParams.state = state;
     });
-    
-    router.run(url);
 
-    if (params.name) contact.name = params.name;
-    if (params.group) contact.addToGroup(project.getOrCreateGroup(params.group));
+    router.run(generatedURL);
 
-}(message.content));
+    return generatedParams;
+
+}(message.content, state.id));
+
+if (params.name)
+    contact.name = params.name;
+
+if (params.groups) {
+    _(params.groups).each(function(group){
+        contact.addToGroup(project.getOrCreateGroup(params.group));
+    });
+}
+
+if (params.state)
+    state.id = params.state;
+
+if (params.reply)
+    sendReply(params.reply);
 
 
